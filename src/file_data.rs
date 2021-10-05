0495553e781e9f1d4cd35e1b6913f94766d6ceb9
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-10-05 12:30:34 -0300

    Start adding stuff to FileData's Display impl

--- src/file_data.rs
+++ src/file_data.rs
@@ -1,4 +1,4 @@
-use std::path::{Path, PathBuf};
+use std::{fmt::Display, fs, path::{Path, PathBuf}};
 
 use filemagic::{Magic, flags::Flags};
 use infer::Type;
@@ -11,10 +11,40 @@ pub struct FileData {
     magic: Option<Magic>
 }
 
+impl Display for FileData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Path: `{0}`", self.path.display())?;
+        
+        if let Some(mime) = self.libmagic_mime_type() {
+            writeln!(f, "Type: `{0}`", mime)?;
+        } else {
+            if let Ok(Some((mime, ext))) = self.mime_type() {
+                writeln!(f, "Type: `{} {}`", mime, ext)?;
+            }
+        }
+
+        writeln!(f, "Bytes: `{}`", self.size())?;
+
+        writeln!(f, "Permissions: `{}`", self.permissions())?;
+
+        if let Some(owner_user) = self.owner_user() {
+            writeln!(f, "Owner's user: `{}`", owner_user)?;
+        }
+
+        if let Some(owner_group) = self.owner_group() {
+            writeln!(f, "Owner's owner group: `{}`", owner_group)?;
+        }
+
+        Ok(())
+    }
+}
+
 pub type MaybeMime<'a> = Option<(&'a str, &'a str)>;
 
 impl FileData {
     pub fn read(path: PathBuf) -> Result<Self> {
+
+        let path = fs::canonicalize(path)?;
 
         let init_magic = || {
             let magic = Magic::open(Default::default()).ok()?;
@@ -37,7 +67,7 @@ impl FileData {
         Ok(infer::get_from_path(&self.path)?.map(mime_and_extension))
     }
 
-    pub fn mime_type2(&self) -> Option<String> {
+    pub fn libmagic_mime_type(&self) -> Option<String> {
         if let Some(ref magic) = self.magic {
             return magic.file(&self.path).ok();
         }
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,10 @@ fn main() {
     if let Some(s) = env::args_os().nth(1) {
         let data = FileData::read(s.into()).unwrap();
 
-        // let _ = dbg!(data.mime_type());
-        dbg!(data.mime_type2());
-        dbg!(data.owner_user());
-        dbg!(data.owner_group());
-        println!("{}", data.permissions());
-        println!("{}", data.size());
+        // // let _ = dbg!(data.mime_type());
+        // dbg!(data.mime_type2());
+        // dbg!(data.owner_user());
+        // dbg!(data.owner_group());
+        println!("{}", data);
     }
 }
